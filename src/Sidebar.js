eabc716390d1ccc484564dde3df48dfe2418a019
Hayat Jemal <htjemal2000@gmail.com>
2022-12-10 00:58:35 +0300

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import SearchIcon from '@material-ui/icons/Search';
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton> 
+            {/* the IconButton it makes clickable */}
+            < DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+          <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreHorizIcon/>
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <sidebarChat />
+        <sidebarChat />
+        <sidebarChat />
+        
+
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5jJkkRuDT-lrgimtMJ2AuUsA2rgDc9qo",
+  authDomain: "whatsapp-mern-71cab.firebaseapp.com",
+  projectId: "whatsapp-mern-71cab",
+  storageBucket: "whatsapp-mern-71cab.appspot.com",
+  messagingSenderId: "598353812632",
+  appId: "1:598353812632:web:c7dd0e0d4926031ce904ed",
+  measurementId: "G-32YZGTE15F",
+};
