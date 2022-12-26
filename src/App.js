51fbf1f77a34fe2cc4880a090768aca45206ead0
Hayat Jemal <htjemal2000@gmail.com>
2022-12-27 01:12:58 +0300

    add pusher function in the app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from "pusher-js"
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('dabc6adfdadee64fbad7', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, [])
   return (
     <div className="app">
       <div className="app__body">
